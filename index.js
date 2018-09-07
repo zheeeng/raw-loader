703f78a54af53f591db994d24db6323f50d5a1be
jiangzheng <jiangzheng@yidian-inc.com>
2018-09-07 23:35:03 +0800

    bump

--- index.js
+++ index.js
@@ -2,12 +2,39 @@
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Tobias Koppers @sokra
 */
+const loaderUtils = require('loader-utils');
+const validateOptions = require('schema-utils');
+
+const schema = {
+  type: 'object',
+  properties: {
+    before: {
+      type: 'string',
+    },
+    after: {
+      type: 'string',
+    },
+    prevent: {
+      type: 'string',
+    },
+  },
+};
+
 module.exports = function rawLoader(source) {
+  const options = loaderUtils.getOptions(this) || {};
+  validateOptions(schema, options, 'raw-loader-bump');
+
   this.value = source;
 
   const json = JSON.stringify(source)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
 
-  return `module.exports = ${json}`;
+  const before = options.before || '';
+  const after = options.after || '';
+  const prevent = options.prevent
+    ? `;window['${options.prevent}'] !== undefined && throw Error();`
+    : '';
+
+  return `module.exports = ${before}${prevent}${json}${after}`;
 };
